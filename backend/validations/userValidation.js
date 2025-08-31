f1f7407225df0582ed29a5e6da35b65cd6abf5b9
FelipeMartins <felipemrts@icloud.com>
2025-08-30 23:16:08 -0400

    add pt-br error message

--- backend/validations/userValidation.js
+++ backend/validations/userValidation.js
@@ -7,15 +7,31 @@ const validateURL = (value, helpers) => {
 };
 
 const createUserSchema = Joi.object({
-  name: Joi.string().min(2).max(30).required(),
-  about: Joi.string().min(2).max(30).required(),
-  avatar: Joi.string().required().custom(validateURL),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  name: Joi.string().min(2).max(30).required().messages({
+    "string.min": "O nome deve ter pelo menos 2 caracteres",
+    "string.max": "O nome deve ter no máximo 30 caracteres",
+  }),
+  about: Joi.string().min(2).max(30).required().messages({
+    "string.min": "O campo 'sobre' deve ter pelo menos 2 caracteres",
+    "string.max": "O campo 'sobre' deve ter no máximo 30 caracteres",
+  }),
+  avatar: Joi.string().required().custom(validateURL).messages({
+    "string.pattern.base": "O link do avatar deve ser uma URL válida",
+  }),
+  email: Joi.string().email().required().messages({
+    "string.email": "O e-mail fornecido é inválido",
+    "any.required": "O e-mail é obrigatório",
+  }),
+  password: Joi.string().min(6).required().messages({
+    "any.required": "A senha é obrigatória",
+  }),
 });
 
 const updateAvatarSchema = Joi.object({
-  avatar: Joi.string().required().custom(validateURL),
+  avatar: Joi.string().required().custom(validateURL).messages({
+    "any.required": "O avatar é obrigatório",
+    "string.pattern.base": "O link do avatar deve ser uma URL válida",
+  }),
 });
 
 module.exports = { createUserSchema, updateAvatarSchema };
